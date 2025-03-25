6954ffc45660443ddc27ee65b9c751e277e33232
ziqin030 <ziqin030@gmail.com>
2025-03-26 00:36:10 +0800

    應該可以直接用吧
    字型、icon有統一

--- contact.php
+++ contact.php
@@ -196,32 +196,21 @@
         <a href="contact.php"><i class="icon fas fa-phone-alt"></i> 聯絡我們</a>
     </div>
     <div class="main-content">
-        <h2 class="mb-4 text-primary">輔大愛校建言捐款系統</h2>
-        <div class="card">
-            <div class="card-header">捐款進度</div>
-            <div class="card-body">
-                <div class="progress">
-                    <div class="progress-bar" style="width: 60%;">60%</div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">📜 最新建言</div>
-                    <div class="card-body">
-                        <p>學生希望改善校內飲水機品質...</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">📊 捐款報表</div>
-                    <div class="card-body">
-                        <p><a href="#" class="text-link">下載最新捐款報表</a></p>
-                    </div>
-                </div>
-            </div>
+        <h2 class="mb-4 text-primary">聯絡我們</h2>
+        <div style="margin-left: 20px; margin-top: 40px; font-size: 18px;">
+            <i class="fa-solid fa-location-dot"></i> 地址：242062 新北市新莊區中正路 510 號 天主教輔仁大學
+            <br><i class="fa-solid fa-phone"></i>生輔組電話：(02)2905-2264
+            <br><i class="fa-solid fa-envelope"></i>生輔組服務信箱：<a
+                href="mailto:sld@mail.fju.edu.tw">sld@mail.fju.edu.tw</a>
+            <br><p>承辦人 邱小姐 電話 02-2905-3031</p>
+            <p>日間部辦公室(于斌樓一樓YP104)</p>
+            <p>學期中：週一至週五 08：00～16：30</p>
+            <p>寒暑假：週一至週四 08：00～16：30</p>
+            <br>
+            <p>進修部辦公室(進修部大樓二樓ES201)</p>
+            <p>學期中：週一至週五 15：00～22：00</p>
+            <p>寒暑假：週一至週四 08：00～16：30</p>
+            <p>(寒暑假作息請參照本校行事曆)</p>
         </div>
     </div>
     <div class="footer">
