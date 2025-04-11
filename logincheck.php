ba748a109b801ed119e1be75bad537d052bce34b
1x919 <ethan930919@gmail.com>
2025-04-11 20:34:36 +0800

    改正admin不行上傳的問題

--- logincheck.php
+++ logincheck.php
@@ -8,6 +8,7 @@ $result = mysqli_query($link, $sql);
 
 if ($User_Name == "admin") {
     $_SESSION['User_Name'] = $User_Name;
+    $_SESSION['User_ID'] = $record['User_ID'];
     $_SESSION['is_admin'] = true; // 設置為管理員
     // 登入成功，刷新父頁面並關閉當前 iframe 頁面
     echo '<script>
