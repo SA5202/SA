8021a6b47d96c9920ce902373a82a37b70a311a7
陳相叡 <sylviachen650046@gmail.com>
2025-04-18 19:30:17 +0800

    校徽

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $is_admin = isset($_SESSION['is_admin']) && $_SESSION['is_admin'];
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/e19963bd49.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500&family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
-
+    <link rel="icon" type="image/png" href="https://www.design-thinking.tw/assets/images/school-logo/FJU.png">
     <style>
         * {
             margin: 0;
