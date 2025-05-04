14124e4e579a76e731bfc3b5bb16a9610e962c25
zyc1412 <a912245389@gmail.com>
2025-05-04 21:21:01 +0800

    刪建言管理

--- index.php
+++ index.php
@@ -335,8 +335,7 @@ $is_admin = isset($_SESSION['is_admin']) && $_SESSION['is_admin'];
             <a href="main.php" target="contentFrame"><i class="icon fas fa-home"></i><span><b> 首頁</b></span></a>
             <a href="news.php" target="contentFrame"><i class="icon fa-solid fa-wrench"></i></i><span><b> 公告管理</b></span></a>
             <a href="news_insert.php" target="contentFrame"><i class="icon fa-solid fa-notes-medical"></i><span><b> 新增公告</b></span></a>
-            <a href="suggestions.php" target="contentFrame"><i class="icon fas fa-scroll"></i><span><b> 建言進度管理</b></span></a>
-            <a href="suggestion_edit.php" target="contentFrame"><i class="icon fas fa-cogs"></i><span><b> 建言管理</b></span></a>
+            <a href="suggestions.php" target="contentFrame"><i class="icon fas fa-cogs"></i><span><b> 建言進度管理</b></span></a>
             <a href="funding_detail.php" target="contentFrame"><i class="icon fas fa-donate"></i><span><b> 募款建言管理</b></span></a>
             <a href="fundingsuggestion.php" target="contentFrame"><i class="icon fas fa-hand-holding-usd"></i><span><b> 新增募款建言</b></span></a>
 
